5bc20383feedb92dbbeae947a06931521c0ed5f6
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-02-25 16:42:46 -0400

    Add nogui

---
+++ src/main/java/me/duncanruns/e4mcbiat/Main.java
@@ -0,0 +1,39 @@
+package me.duncanruns.e4mcbiat;
+
+import me.duncanruns.e4mcbiat.gui.E4mcBiatGUI;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (Arrays.stream(args).anyMatch(string -> string.toLowerCase().contains("nogui"))) {
+            runWithNoGUI();
+        } else {
+            E4mcBiatGUI.main(args);
+        }
+    }
+
+    private static void runWithNoGUI() throws IOException {
+        E4mcClient e4mcClient = new E4mcClient(d -> System.out.println("Connected, domain: " + d), m -> System.out.println("e4mc broadcast: " + m));
+        Thread inputThread = new Thread(() -> {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNextLine()) {
+                    if (scanner.nextLine().trim().equalsIgnoreCase("stop")) {
+                        e4mcClient.close();
+                        return;
+                    } else {
+                        System.out.println("Unknown command.");
+                    }
+                }
+            } catch (Exception e) {
+                e4mcClient.close();
+            }
+        });
+        inputThread.setDaemon(true);
+        inputThread.start();
+        e4mcClient.run();
+    }
+}
